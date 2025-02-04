75991c527b685d3d0a29c86c231f7a4c2572ae53
Sunny Lin <linsunny911@yahoo.com>
2025-02-04 17:14:51 -0500

    Created and modified accounts file and added base.html to moviestore\templates

--- moviestore/settings.py
+++ moviestore/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'home',
     'movies',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'moviestore.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'moviestore/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
