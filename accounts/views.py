1258e308907c486ff273424f8cf54b1001742117
Sunny Lin <linsunny911@yahoo.com>
2025-02-11 17:25:32 -0500

    Fixed bugs and finished chapt 9

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import redirect
 from .forms import CustomUserCreationForm, CustomErrorList
-from django.contrib.auth import login as auth_login, authenticate
+from django.contrib.auth import login as auth_login, authenticate, logout as auth_logout
 from django.contrib.auth.decorators import login_required
 
 @login_required
@@ -14,8 +14,7 @@ def login(request):
     template_data = {}
     template_data['title'] = 'Login'
     if request.method == 'GET':
-        return render(request, 'accounts/login.html',
-            {'template_data': template_data})
+        return render(request, 'accounts/login.html', {'template_data': template_data})
     elif request.method == 'POST':
         user = authenticate(
             request,
@@ -24,8 +23,7 @@ def login(request):
         )
         if user is None:
             template_data['error'] = 'The username or password is incorrect.'
-            return render(request, 'accounts/login.html',
-                {'template_data': template_data})
+            return render(request, 'accounts/login.html', {'template_data': template_data})
         else:
             auth_login(request, user)
             return redirect('accounts.login')
@@ -35,8 +33,7 @@ def signup(request):
     template_data['title'] = 'Sign Up'
     if request.method == 'GET':
         template_data['form'] = CustomUserCreationForm()
-        return render(request, 'accounts/signup.html',
-            {'template_data': template_data})
+        return render(request, 'accounts/signup.html', {'template_data': template_data})
     elif request.method == 'POST':
         form = CustomUserCreationForm(request.POST, error_class=CustomErrorList)
         if form.is_valid():
@@ -44,6 +41,5 @@ def signup(request):
             return redirect('home.index')
         else:
             template_data['form'] = form
-            return render(request, 'accounts/signup.html',
-                {'template_data': template_data})
+            return render(request, 'accounts/signup.html', {'template_data': template_data})
 
